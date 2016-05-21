ad3eb9126a90c4b787578a6ac32d509860e86652
Senorsen <senorsen.zhang@gmail.com>
2016-05-21 12:28:52 +0800

    copied some configurations...

--- webpack.config.js
+++ webpack.config.js
@@ -20,8 +20,27 @@ module.exports = {
                 query: {
                     presets: ["es2015"]
                 }
+            },
+            {
+                test: /\.(css|less)$/,
+                loader: ExtractTextPlugin.extract('style', css-raw!autoprefixer!less')
+            },
+            {
+                test: /\.(html|svg)$/,
+                loader: 'raw'
             }
         ]
+    },
+    resolve: {
+        moduleDirectories: ['src', 'directives', 'components', 'node_modules', 'lib']
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css', {allChunks: true})
+    ],
+    devServer: {
+        port: 3333,
+        inline: true,
+        host: '0.0.0.0'
     },
     debug: true
 };
