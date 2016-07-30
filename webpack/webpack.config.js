13c83cd130ea75f5093e92999d5490a762987abf
Senorsen <senorsen.zhang@gmail.com>
2016-07-30 19:40:04 +0800

    Add html plugin config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,13 +3,19 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import webpack from 'webpack';
 import precss from 'precss';
 import autoprefixer from 'autoprefixer';
+import HtmlPlugin from 'html-webpack-plugin';
 
 import babelConfig from './babel.config';
 
+const production = process.env.NODE_ENV === 'production';
+
 export default {
-    entry: './src/entry.js',
+    context: path.join(__dirname, '../src'),
+    entry: {
+        src: './entry'
+    },
     output: {
-        path: './public',
+        path: path.join(__dirname, '../public'),
         filename: 'bundle.js'
     },
     module: {
@@ -19,8 +25,12 @@ export default {
                 loader: ExtractTextPlugin.extract('style!css!postcss')
             },
             {
-                test: /\.(html|png|svg)$/,
+                test: /\.(png|svg)$/,
                 loader: 'raw'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html'
             },
             {
                 test: /\.js$/,
@@ -33,6 +43,17 @@ export default {
     plugins: [
         new ExtractTextPlugin('style.css', {
             allChunks: true
+        }),
+        new HtmlPlugin({
+            template: 'index.html',
+            filename: 'index.html',
+            inject: 'body',
+            hash: true,
+            minify: {
+                caseSensitive: true,
+                collapseWhitespace: production,
+                conservativeCollapse: production
+            }
         })
     ],
     postcss: () => [precss, autoprefixer],
